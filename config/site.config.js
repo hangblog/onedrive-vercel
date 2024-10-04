90f4e48fd808aa468905546ccf21f1c7bb6192d2
hangblog <mzhangrx@icloud.com>
2024-10-04 23:20:49 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,9 +52,10 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
+      name: 'GitHub',
+      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
     },
   ],
-
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
   // without timezone and replacing T with space.
